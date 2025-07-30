c701b09e392e7d6edf97676c252ab151fffcbbd6
clair <clair.wang102@gmail.com>
2025-07-30 11:00:01 -0400

    updates script.js

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 //Press a button to choose your path
 //See the README file for more information
 
-
 /* VARIABLES */
 let font1;
 
-// Music
 let bgMusic;
 let isMuted = false;
 let soundOn;
 let musicButton;
 
-// Buttons
+
 let enterButton;
 
 let a1Button;
 let a2Button;
 let b1Button;
 let b2Button;
+let backButton;
 
 let screen = 0;
 
-// Icons
 let ball;
 let fish;
 let cat;
@@ -29,6 +27,13 @@ let page;
 let picker;
 let star1;
 let star2;
+
+let virus;
+
+let pointer;
+
+let smiski;
+let backSmiski;
 
 function preload() {
   font1 = loadFont('FONTS/MYFONT.ttf');
@@ -47,8 +52,7 @@ function setup() {
   textAlign(CENTER);
   textSize(20);
   noStroke();
-
-
+  
   ball = new Sprite();
   ball.img = loadImage('ICONS/BALL.png');
   ball.x = 160; 
@@ -63,7 +67,7 @@ function setup() {
 
   cat = new Sprite();
   cat.img = loadImage('ICONS/cat.png');
-  cat.x = 475; 
+  cat.x = 410; 
   cat.y = 130; 
   cat.visible = true;
 
@@ -92,8 +96,20 @@ function setup() {
   star2.y = 270; 
   star2.visible = true;
 
+  virus = new Sprite();
+  virus.img = loadImage('ICONS/virus.png');
+  virus.x = width/2; 
+  virus.y = 270; 
+  virus.visible = false;
 
+  smiski = new Sprite();
+  smiski.img = loadImage('ICONS/smiski.png');
+  smiski.x = width - 40;
+  smiski.y = 100;
+  smiski.collider = 'k';
+  smiski.visible = true;
 
+  
   // // MUSIC BUTTON SPRITEEEE
   musicButton = new Sprite();
   musicButton.img = soundOn;
@@ -103,59 +119,29 @@ function setup() {
   musicButton.h = 100; 
   musicButton.collider = 'k';
 
-
-  // Set up the home screen
-  background("#fffff")
-  fill("#769c8b");
-  rect(0, 0, 600, 400, 20); 
-
-  fill(255);
-  textFont(font1);
-  textSize(40);
-  text("404 :( \nPATH NOT FOUND", 
-       width / 2, height / 2 - 60);
-  textSize(10);
-  text("you tried to visit your favorite online game. \nbut instead of loading, \nyour browswer started blinking. \nyou're... inside the internet?", width/2, height/2 + 10);
-
-  textSize(20);
-  textFont(font1);
-  noFill();
-
-
-
-      // CUSTOMIZATION FOR HOME SCREEN
-
-      fill("#f0dcc0"); 
-      rect(0, 0, 600, 40, 20, 20, 0, 0);
-      fill("#b3342b");
-      ellipse(30, 20, 20, 20);
-      fill("#e3bf4b");
-      ellipse(60, 20, 20, 20);
-      fill("#a6cf88");
-      ellipse(90, 20, 20, 20);
-
-
-
-
   // Create buttons for all screens
-  enterButton = new Sprite( width /  2 , height / 2 + 90);  
+  enterButton = new Sprite(-100, -100);
   a1Button = new Sprite( -200, -200);  
   a2Button = new Sprite( -200, -200);
   b1Button = new Sprite( - 100, -100);
   b2Button = new Sprite( - 150, -150);
+  
+  backButton = new Sprite(-100, -100);
+  backButton.rotation = 0;
+  backButton.img = loadImage('ICONS/back.png');
+
+  showScreen0();
 
 }
 
 
+
 /* DRAW LOOP REPEATS */
 function draw() {
-
   if (!isMuted){
     musicButton.rotation += 1;
-  } else {
-
   }
-
+  
   // Display enter button
   enterButton.w = 100;
   enterButton.h = 50;
@@ -199,6 +185,10 @@ function draw() {
     }
   }
 
+  if ( smiski.mouse.presses() ){
+    showHomepage();
+  }
+  
   if (screen == 1) {
 
     if (a1Button.mouse.presses()) {
@@ -221,13 +211,122 @@ function draw() {
       showScreen4();
       screen = 4
     }
+  } 
+
+  if (backButton.mouse.presses()){
+    showScreen0();
   }
+
+  enterButton.rotation = 0;
+  backButton.rotation = 0;
 
 }
 
+function showHomepage(){
+  print("showing homepage");
+  
+  fill("#706e68");
+  rect(0, 0, 600, 400, 20); 
+  drawTopBar();
+
+  ball.visible = false;
+  fish.visible = false;
+  cat.visible = false;
+  page.visible = false;
+  picker.visible = false;
+  star1.visible = false;
+  star2.visible = false;
+
+  enterButton.pos = {x: -200, y: -200};
+  
+
+  stroke("faf7ed");
+  fill("#6c7d59");
+  rect(90, 130, 160, 100, 20);
+
+  fill("#6c7d59");
+  rect(297, 130, 220, 30, 10);
+
+  fill("#6c7d59");
+  rect(297, 170, 220, 110, 20);
+  
+  strokeWeight(2);
+  stroke("#faf7ed");
+  fill("#faf7ed");
+  textSize(30);
+  text("about me: clair w.", width/2, 90);
+
+  strokeWeight(0);
+  textSize(12);
+
+  strokeWeight(1);
+  text("my fav programming \nlanguages...", 172, 150);
+  text("hs graduation year: 2028", 402, 150);
+  text("favorite hobbies:", 410, 190);
+
+  strokeWeight(0);
+  text("java, c++, js\n special shout-out: \nlatex", 171, 184);
+  text("playing my oboe - math \ncoding - listening to music \npiano - traveling", 410, 220);
+
+  backButton.pos = { x: 60, y: 360 };
+  
+}
 
 
 /* FUNCTIONS TO DISPLAY SCREENS */
+function showScreen0(){
+
+  print("back to screen 0");
+  
+  // Set up the home screen
+  background("#fffff");
+  fill("#769c8b");
+  rect(0, 0, 600, 400, 20); 
+
+  ball.visible = true;
+  fish.visible = true;
+  cat.visible = true;
+  page.visible = true;
+  picker.visible = true;
+  star1.visible = true;
+  star2.visible = true;
+  smiski.visible = true;
+
+  
+  enterButton.pos = { x: width / 2, y: height / 2 + 90 };
+  a1Button.pos = { x: -100, y: -100};
+  a2Button.pos = { x: -100, y: -100};
+  b1Button.pos = { x: -100, y: -100};
+  b2Button.pos = { x: -100, y: -100};
+  virus.pos = { x: -100, y: -100};
+  backButton.pos = {x: -100, y: -100};
+
+
+  fill(255);
+  textFont(font1);
+  textSize(40);
+  text("404 :( \nPATH NOT FOUND", 
+       width / 2, height / 2 - 60);
+  textSize(10);
+  text("you tried to visit your favorite online game. \nbut instead of loading, \nyour browswer started blinking. \nyou're... inside the internet?", width/2, height/2 + 10);
+
+  textSize(20);
+  textFont(font1);
+  noFill();
+
+      // CUSTOMIZATION FOR HOME SCREEN
+
+      fill("#f0dcc0"); 
+      rect(0, 0, 600, 40, 20, 20, 0, 0);
+      fill("#b3342b");
+      ellipse(30, 20, 20, 20);
+      fill("#e3bf4b");
+      ellipse(60, 20, 20, 20);
+      fill("#a6cf88");
+      ellipse(90, 20, 20, 20);
+
+}
+
 function showScreen1(){
 
   fill("#071e42");
@@ -249,6 +348,7 @@ function showScreen1(){
   star1.visible = false;
   star2.visible = false;
 
+  smiski.visible = false;
 
   // Button 1: Flashy Ad Portal
   a1Button.pos = { x: width / 2 - 90, y: height / 2 + 100 };
@@ -282,7 +382,7 @@ function showScreen2(){
   picker.visible = false;
   star1.visible = false;
   star2.visible = false;
-
+  
   // Move extra buttons off screen
   a1Button.pos = { x: -200, y: -200 };
   a2Button.pos = { x: -50, y: -50 };
@@ -309,8 +409,8 @@ function showScreen3() {
   rect(0, 0, 600, 400, 20); 
   drawTopBar();
 
-  text("you installed 'SystemCleaner.EXE'.\nyour screen fades to black...\n\n\n\n\n\n\n\n\n\n", width / 2, height / 2 - 60);
-  text("\n\n\n\nyou’ve been logged out — forever.", width / 2, height / 2);
+  text("you installed 'SystemCleaner.EXE'.\nyour screen fades to black...\n\n\n\n\n\n\n\n\n\n", width / 2, height / 2 - 65);
+  text("\n\n\n\n\n\n\nyou’ve been logged out — forever.", width / 2, height / 2);
 
   ball.visible = false;
   fish.visible = false;
@@ -319,7 +419,11 @@ function showScreen3() {
   picker.visible = false;
   star1.visible = false;
   star2.visible = false;
+  
+  virus.visible = true;
 
+  backButton.pos = { x: 60, y: 360 };
+  
   // Move extra buttons off screen
   b1Button.pos = { x: -100, y: -100 };
   b2Button.pos = { x: -150, y: -150 };
@@ -345,6 +449,8 @@ function showScreen4() {
   // Move extra buttons off screen
   b1Button.pos = { x: -100, y: -100 };
   b2Button.pos = { x: -150, y: -150 };
+
+  backButton.pos = { x: 60, y: 360 };
 }
 
 function showScreen5(){
@@ -367,12 +473,16 @@ function showScreen5(){
   // Move extra buttons off screen
   a1Button.pos = { x: -200, y: -200 };
   a2Button.pos = { x: -50, y: -50 };
+
+  backButton.pos = { x: 60, y: 360 };
+
 }
+
 
 
 function drawTopBar() {
   // Rounded top bar
-  fill("#f0dcc0");
+  fill("#eedec7ff");
   rect(0, 0, 600, 40, 20, 20, 0, 0);
 
   // Browser-style window buttons
