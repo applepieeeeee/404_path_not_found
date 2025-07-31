dc5de363ed4f9aab5f630383fa7abda35aded777
clair <clair.wang102@gmail.com>
2025-07-31 13:50:20 -0400

    decided i liked some of the rotating bc it fits the theme

--- display.js
+++ display.js
@@ -216,11 +216,9 @@ function draw() {
 
   enterButton.rotation = 0;
   backButton.rotation = 0;
-  a1Button.rotation = 0;
-  b1Button.rotation = 0;
   a2Button.rotation = 0;
   b2Button.rotation = 0;
-  
+
 }
 
 
